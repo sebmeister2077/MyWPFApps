bbded5a27821fe53a31c743487712b55ff231d5a
sebask <sebaskolster@gmail.com>
2021-02-28 15:24:56 +0200

    Fixed some minor problems and added more UI functionalities

--- BBG-BigBallGame/Simulation.Designer.cs
+++ BBG-BigBallGame/Simulation.Designer.cs
@@ -39,13 +39,26 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtratioReg = new System.Windows.Forms.TextBox();
-            this.txtratioRep = new System.Windows.Forms.TextBox();
             this.txtratioMon = new System.Windows.Forms.TextBox();
+            this.txtratioRep = new System.Windows.Forms.TextBox();
+            this.txtratioReg = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbtnGenMode = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rbtnCountMode = new System.Windows.Forms.RadioButton();
+            this.txbxReg = new System.Windows.Forms.TextBox();
+            this.txbxRep = new System.Windows.Forms.TextBox();
+            this.txbxMon = new System.Windows.Forms.TextBox();
+            this.btnPauseResume = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trckbarSpeed)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox
@@ -71,7 +84,7 @@
             this.trckbarSpeed.Location = new System.Drawing.Point(1306, 29);
             this.trckbarSpeed.Minimum = 1;
             this.trckbarSpeed.Name = "trckbarSpeed";
-            this.trckbarSpeed.Size = new System.Drawing.Size(104, 45);
+            this.trckbarSpeed.Size = new System.Drawing.Size(119, 45);
             this.trckbarSpeed.TabIndex = 2;
             this.trckbarSpeed.Value = 10;
             this.trckbarSpeed.Scroll += new System.EventHandler(this.trckbarSpeed_Scroll);
@@ -88,7 +101,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1307, 77);
+            this.label2.Location = new System.Drawing.Point(6, 44);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 13);
             this.label2.TabIndex = 4;
@@ -96,17 +109,20 @@
             // 
             // txbxballs
             // 
-            this.txbxballs.Location = new System.Drawing.Point(1306, 93);
+            this.txbxballs.Enabled = false;
+            this.txbxballs.Location = new System.Drawing.Point(9, 60);
             this.txbxballs.Name = "txbxballs";
             this.txbxballs.Size = new System.Drawing.Size(90, 20);
             this.txbxballs.TabIndex = 5;
-            this.txbxballs.Text = "20";
+            this.txbxballs.Text = "14";
+            this.txbxballs.TextChanged += new System.EventHandler(this.txbxballs_TextChanged);
             // 
             // btnreset
             // 
-            this.btnreset.Location = new System.Drawing.Point(1306, 182);
+            this.btnreset.Font = new System.Drawing.Font("Arial", 13.25F);
+            this.btnreset.Location = new System.Drawing.Point(1306, 377);
             this.btnreset.Name = "btnreset";
-            this.btnreset.Size = new System.Drawing.Size(99, 42);
+            this.btnreset.Size = new System.Drawing.Size(119, 42);
             this.btnreset.TabIndex = 6;
             this.btnreset.Text = "Reset";
             this.btnreset.UseVisualStyleBackColor = true;
@@ -147,46 +163,170 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Location = new System.Drawing.Point(1306, 119);
+            this.groupBox1.Location = new System.Drawing.Point(9, 86);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(102, 57);
             this.groupBox1.TabIndex = 11;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Ball Type Ratio";
             // 
-            // txtratioReg
-            // 
-            this.txtratioReg.Location = new System.Drawing.Point(6, 32);
-            this.txtratioReg.Name = "txtratioReg";
-            this.txtratioReg.Size = new System.Drawing.Size(27, 20);
-            this.txtratioReg.TabIndex = 11;
-            this.txtratioReg.Text = "6";
-            // 
-            // txtratioRep
-            // 
-            this.txtratioRep.Location = new System.Drawing.Point(38, 32);
-            this.txtratioRep.Name = "txtratioRep";
-            this.txtratioRep.Size = new System.Drawing.Size(28, 20);
-            this.txtratioRep.TabIndex = 12;
-            this.txtratioRep.Text = "3";
-            // 
             // txtratioMon
             // 
+            this.txtratioMon.Enabled = false;
             this.txtratioMon.Location = new System.Drawing.Point(72, 32);
             this.txtratioMon.Name = "txtratioMon";
             this.txtratioMon.Size = new System.Drawing.Size(27, 20);
             this.txtratioMon.TabIndex = 13;
             this.txtratioMon.Text = "1";
+            this.txtratioMon.TextChanged += new System.EventHandler(this.txtratioMon_TextChanged);
+            // 
+            // txtratioRep
+            // 
+            this.txtratioRep.Enabled = false;
+            this.txtratioRep.Location = new System.Drawing.Point(38, 32);
+            this.txtratioRep.Name = "txtratioRep";
+            this.txtratioRep.Size = new System.Drawing.Size(28, 20);
+            this.txtratioRep.TabIndex = 12;
+            this.txtratioRep.Text = "3";
+            this.txtratioRep.TextChanged += new System.EventHandler(this.txtratioRep_TextChanged);
+            // 
+            // txtratioReg
+            // 
+            this.txtratioReg.Enabled = false;
+            this.txtratioReg.Location = new System.Drawing.Point(6, 32);
+            this.txtratioReg.Name = "txtratioReg";
+            this.txtratioReg.Size = new System.Drawing.Size(27, 20);
+            this.txtratioReg.TabIndex = 11;
+            this.txtratioReg.Text = "6";
+            this.txtratioReg.TextChanged += new System.EventHandler(this.txtratioReg_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbtnGenMode);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.groupBox1);
+            this.groupBox2.Controls.Add(this.txbxballs);
+            this.groupBox2.Location = new System.Drawing.Point(1306, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(119, 149);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Random Generation Mode";
+            // 
+            // rbtnGenMode
+            // 
+            this.rbtnGenMode.AutoSize = true;
+            this.rbtnGenMode.Location = new System.Drawing.Point(6, 28);
+            this.rbtnGenMode.Name = "rbtnGenMode";
+            this.rbtnGenMode.Size = new System.Drawing.Size(14, 13);
+            this.rbtnGenMode.TabIndex = 12;
+            this.rbtnGenMode.TabStop = true;
+            this.rbtnGenMode.UseVisualStyleBackColor = true;
+            this.rbtnGenMode.CheckedChanged += new System.EventHandler(this.rbtnGenMode_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txbxMon);
+            this.groupBox3.Controls.Add(this.txbxRep);
+            this.groupBox3.Controls.Add(this.txbxReg);
+            this.groupBox3.Controls.Add(this.rbtnCountMode);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(1306, 223);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(119, 100);
+            this.groupBox3.TabIndex = 13;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Fixed Ball Count Mode";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Reg";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(39, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Rep";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(72, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(28, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Mon";
+            // 
+            // rbtnCountMode
+            // 
+            this.rbtnCountMode.AutoSize = true;
+            this.rbtnCountMode.Checked = true;
+            this.rbtnCountMode.Location = new System.Drawing.Point(9, 28);
+            this.rbtnCountMode.Name = "rbtnCountMode";
+            this.rbtnCountMode.Size = new System.Drawing.Size(14, 13);
+            this.rbtnCountMode.TabIndex = 13;
+            this.rbtnCountMode.TabStop = true;
+            this.rbtnCountMode.UseVisualStyleBackColor = true;
+            this.rbtnCountMode.CheckedChanged += new System.EventHandler(this.rbtnCountMode_CheckedChanged);
+            // 
+            // txbxReg
+            // 
+            this.txbxReg.Location = new System.Drawing.Point(9, 60);
+            this.txbxReg.Name = "txbxReg";
+            this.txbxReg.Size = new System.Drawing.Size(27, 20);
+            this.txbxReg.TabIndex = 14;
+            this.txbxReg.Text = "7";
+            this.txbxReg.TextChanged += new System.EventHandler(this.txbxReg_TextChanged);
+            // 
+            // txbxRep
+            // 
+            this.txbxRep.Location = new System.Drawing.Point(42, 60);
+            this.txbxRep.Name = "txbxRep";
+            this.txbxRep.Size = new System.Drawing.Size(27, 20);
+            this.txbxRep.TabIndex = 15;
+            this.txbxRep.Text = "3";
+            this.txbxRep.TextChanged += new System.EventHandler(this.txbxRep_TextChanged);
+            // 
+            // txbxMon
+            // 
+            this.txbxMon.Location = new System.Drawing.Point(75, 60);
+            this.txbxMon.Name = "txbxMon";
+            this.txbxMon.Size = new System.Drawing.Size(27, 20);
+            this.txbxMon.TabIndex = 16;
+            this.txbxMon.Text = "1";
+            this.txbxMon.TextChanged += new System.EventHandler(this.txbxMon_TextChanged);
+            // 
+            // btnPauseResume
+            // 
+            this.btnPauseResume.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.btnPauseResume.ForeColor = System.Drawing.Color.Red;
+            this.btnPauseResume.Location = new System.Drawing.Point(1306, 329);
+            this.btnPauseResume.Name = "btnPauseResume";
+            this.btnPauseResume.Size = new System.Drawing.Size(119, 42);
+            this.btnPauseResume.TabIndex = 14;
+            this.btnPauseResume.Text = "Pause";
+            this.btnPauseResume.UseVisualStyleBackColor = true;
+            this.btnPauseResume.Click += new System.EventHandler(this.btnPauseResume_Click);
             // 
             // BBG
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1414, 801);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(1430, 801);
+            this.Controls.Add(this.btnPauseResume);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.btnreset);
-            this.Controls.Add(this.txbxballs);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.trckbarSpeed);
             this.Controls.Add(this.pictureBox1);
@@ -199,6 +339,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.trckbarSpeed)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,6 +364,17 @@
         private System.Windows.Forms.TextBox txtratioMon;
         private System.Windows.Forms.TextBox txtratioRep;
         private System.Windows.Forms.TextBox txtratioReg;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbtnGenMode;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txbxMon;
+        private System.Windows.Forms.TextBox txbxRep;
+        private System.Windows.Forms.TextBox txbxReg;
+        private System.Windows.Forms.RadioButton rbtnCountMode;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPauseResume;
     }
 }
 
--- BBG-BigBallGame/Simulation.cs
+++ BBG-BigBallGame/Simulation.cs
@@ -21,6 +21,9 @@ namespace BBG_BigBallGame
         Bitmap bmp;
         Graphics grp;
         Stack<Ball> remove;
+        Queue<(Ball,int)> buffer;//buffer used to delay ball.RemoveVisitor(theOtherBall.id);//using Tuple
+        bool randomMode = false;
+        int[] ratios;
         public BBG()
         {
             InitializeComponent();
@@ -30,17 +33,29 @@ namespace BBG_BigBallGame
             bmp = new Bitmap(1300, 800);
             remove = new Stack<Ball>();
             lista = new List<Ball>();
-            Initialize(20, new ushort[] { 6, 3, 1 }) ;//amount of balls as parameter
+            buffer = new Queue<(Ball, int)>();
+            ratios = new int[3] {6,3,1 };
+            regular = int.Parse(txbxReg.Text);
+            repelent = int.Parse(txbxRep.Text);
+            monster = int.Parse(txbxMon.Text);
+            Initialize(ReturnBallCount()) ;//amount of balls as parameter
             Start();
         }
 
-        private void Initialize(int n,ushort[] ratios)
+        private void Initialize(int n,bool random=false)
         {
             remove = new Stack<Ball>();
             lista = new List<Ball>();
-            regular = 0;
-            repelent = 0;
-            monster = 0;
+            int auxm, auxrp, auxrg;//in caz ca este cu nr fix de bile x,y,si z
+            if (random)
+            {
+                regular = 0;
+                repelent = 0;
+                monster = 0;
+            } 
+            auxm = monster;
+            auxrp = repelent;
+            auxrg = regular;
             while (n-- > 0)
             {
                 Ball b = new Ball(n+1);
@@ -49,17 +64,42 @@ namespace BBG_BigBallGame
                 b.Color = Color.FromArgb(200,(byte)(rnd.Next() % 255), (byte)(rnd.Next() % 255), (byte)(rnd.Next() % 255));
                 b.Angle = rnd.Next() % 361 - 181;//interval: [-180,180]
                 b.Speed = (rnd.Next() % 5)*3 + 4;
-                int aux = rnd.Next() % (ratios[0]+ratios[1]+ratios[2]);
-                if (aux <ratios[2])
-                { b.Type = "MonBall"; b.Speed = 0;monster++;b.Raza = Math.Max(6, b.Raza / 4); }// sanse: 1/10  +raza mai mica
+                if (random == true)
+                {
+                    int aux = rnd.Next() % (ratios[0] + ratios[1] + ratios[2]);
+                    if (aux < ratios[2])
+                    { b.Type = "MonBall"; b.Speed = 0; monster++; b.Raza = Math.Max(6, b.Raza / 4); }// sanse: 1/10  +raza mai mica
+                    else
+                        if (aux < ratios[2] + ratios[1])
+                    { b.Type = "RepBall"; repelent++; }//  3/10
+                    else
+                    { b.Type = "RegBall"; regular++; }//   6/10
+                }
                 else
-                    if (aux < ratios[2]+ratios[1])
-                { b.Type = "RepBall"; repelent++; }//  3/10
-                else
-                { b.Type = "RegBall"; regular++; }//   6/10
+                {
+                    if (auxm-- > 0)
+                    { b.Type = "MonBall"; b.Speed = 0; b.Raza = Math.Max(6, b.Raza / 4); }
+                    else
+                        if (auxrp-- > 0)
+                        b.Type = "RepBall";
+                    else
+                        b.Type = "RegBall";
+
+                }
                 lista.Add(b);
             }
         }
+
+        private int ReturnBallCount()
+        {
+            if (!randomMode)
+            {
+                return monster + repelent + regular;
+            }
+            else
+                return ushort.Parse(txbxballs.Text);
+        }
+
         private void Start()
         {
             t = new Timer();
@@ -76,7 +116,7 @@ namespace BBG_BigBallGame
             {
                 foreach (var bila in lista.ToList())
                 {
-                    if (bila.Raza < 1)
+                    if (bila.Raza < 2)
                     { bila.Id=0;remove.Push(bila); repelent--; continue; }//numai bilele repelent se micsoreaza,restul Doar cresc,raman la fel sau dispar
                     if(bila.Id==0)
                     { remove.Push(bila);continue; }//bila eliminata(mancata),doar bilele regular sunt mancate
@@ -113,13 +153,14 @@ namespace BBG_BigBallGame
                                 bilaVecina.Touched(bila.Id);
                             }
                             else
-                                bila.RemoveVisitor(bilaVecina.Id);//resets the collision EVENT to Enabled
-                    
+                                buffer.Enqueue((bila, bilaVecina.Id));
                 }
             }
-            //Remove deleted balls from lista
+            //Remove deleted balls from lista and resets the collision EVENT to Enabled
             while (remove.Count > 0)
                 lista.Remove(remove.Pop());
+            while (buffer.Count > 0)
+                buffer.Peek().Item1.RemoveVisitor(buffer.Dequeue().Item2);
             //Update canvas
             grp = Graphics.FromImage(bmp);
             grp.Clear(Color.AntiqueWhite);
@@ -157,27 +198,12 @@ namespace BBG_BigBallGame
         private void btnreset_Click(object sender, EventArgs e)
         {
             t.Stop();
-            ushort balls;
-            if (ushort.TryParse(txbxballs.Text, out balls))
-            {
-                if (balls <= 0 || balls > 50)
-                { 
-                    balls = Math.Max((ushort)2, balls);
-                    balls = Math.Min((ushort)50, balls);
-                    txbxballs.Text = balls.ToString();
-                }
-                ushort ratReg, ratRep, ratMon;//ratios
-                if (!ushort.TryParse(txtratioReg.Text, out ratReg))
-                    return;
-                if (!ushort.TryParse(txtratioRep.Text, out ratRep))
-                    return;
-                if (!ushort.TryParse(txtratioMon.Text, out ratMon))
-                    return;
-                Initialize(balls,new ushort[] {ratReg,ratRep,ratMon});
-                Start();
-            }   
+            monster = int.Parse(txbxMon.Text);
+            repelent = int.Parse(txbxRep.Text);
+            regular = int.Parse(txbxReg.Text);
+            Initialize(ReturnBallCount(),randomMode);
+            Start();
         }
-
         private double Distanta(Punct p1,Punct p2)
         {
             return Math.Sqrt((p2.X - p1.X) * (p2.X - p1.X) + (p2.Y - p1.Y) * (p2.Y - p1.Y));
@@ -240,5 +266,130 @@ namespace BBG_BigBallGame
                     b2.Raza /= 2;
             }
         }
+        #region Mode Select
+        private void rbtnCountMode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (randomMode==false &&rbtnCountMode.Checked == false)
+                rbtnCountMode.Checked = true;//cant uncheck mode,you can only switch mode by checking the other radio btn mode
+            else
+            if(randomMode==true&&rbtnCountMode.Checked==true)
+            {//switch
+                randomMode = false;
+                rbtnGenMode.Checked = false;
+                txbxballs.Enabled = false;
+                txtratioMon.Enabled = false;
+                txtratioRep.Enabled = false;
+                txtratioReg.Enabled = false;
+                txbxMon.Enabled = true;
+                txbxRep.Enabled = true;
+                txbxReg.Enabled = true;
+            }
+        }
+        private void rbtnGenMode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (randomMode==true&& rbtnGenMode.Checked == false)
+                rbtnGenMode.Checked = true;//cant uncheck mode,you can only switch mode by checking the other radio btn mode
+            else
+            if(randomMode==false&&rbtnGenMode.Checked==true)
+            {//switch
+                randomMode = true;
+                rbtnCountMode.Checked = false;
+                txbxballs.Enabled = true;
+                txbxMon.Enabled = false;
+                txbxRep.Enabled = false;
+                txbxReg.Enabled = false;
+                txtratioMon.Enabled = true;
+                txtratioRep.Enabled = true;
+                txtratioReg.Enabled = true;
+            }
+        }
+        #endregion
+        #region TextBoxes on the spot verification
+        //automatically verifies in place(while user is inputting) if if is a valid number and replaces it if necessary
+
+        private void txbxballs_TextChanged(object sender, EventArgs e)
+        {
+            if(txbxballs.Text!="")
+            txbxballs.Text = Min(50, Max(1, int.Parse(txbxballs.Text))).ToString();
+        }
+        private void txtratioReg_TextChanged(object sender, EventArgs e)
+        {
+            if (txtratioReg.Text != "")
+            {
+                txtratioReg.Text = Min(100, Max(0, int.Parse(txtratioReg.Text))).ToString();
+                ratios[0] = int.Parse(txtratioReg.Text);
+            }
+            else ratios[0] = 0;
+        }
+        private void txtratioRep_TextChanged(object sender, EventArgs e)
+        {
+            if (txtratioRep.Text != "")
+            {
+                txtratioRep.Text = Min(100, Max(0, int.Parse(txtratioRep.Text))).ToString();
+                ratios[1] = int.Parse(txtratioRep.Text);
+            }
+            else ratios[1] = 0;
+        }
+        private void txtratioMon_TextChanged(object sender, EventArgs e)
+        {
+            if (txtratioMon.Text != "")
+            {
+                txtratioMon.Text = Min(100, Max(0, int.Parse(txtratioMon.Text))).ToString();
+                ratios[2] = int.Parse(txtratioMon.Text);
+            }
+            else ratios[2] = 0;
+        }
+        private void txbxReg_TextChanged(object sender, EventArgs e)
+        {
+            if (txbxReg.Text != "")
+            {
+                txbxReg.Text = Min(50, Max(0, int.Parse(txbxReg.Text))).ToString();
+                regular = int.Parse(txbxReg.Text);
+            }
+            else regular = 0;
+        }
+
+        private void txbxRep_TextChanged(object sender, EventArgs e)
+        {
+            if (txbxRep.Text != "")
+            {
+                txbxRep.Text = Min(50, Max(0, int.Parse(txbxRep.Text))).ToString();
+                repelent = int.Parse(txbxRep.Text);
+            }
+            else repelent = 0;
+        }
+
+        private void txbxMon_TextChanged(object sender, EventArgs e)
+        {
+            if (txbxMon.Text != "")
+            {
+                txbxMon.Text = Min(30, Max(0, int.Parse(txbxMon.Text))).ToString();
+                monster = int.Parse(txbxMon.Text);
+            }
+            else monster = 0;
+        }
+
+        #endregion
+        private void btnPauseResume_Click(object sender, EventArgs e)
+        {
+            if(btnPauseResume.Text=="Pause")
+            {
+                btnPauseResume.Text = "Resume";
+                btnPauseResume.ForeColor = Color.Green;
+                t.Stop();
+            }
+            else
+            {
+                t.Start();
+                btnPauseResume.Text = "Pause";
+                btnPauseResume.ForeColor = Color.Red;
+            }
+        }
+        public int Min(int a,int b)
+        {
+            return Math.Min(a, b);
+        }
+        public int Max(int a,int b)
+        { return Math.Max(a, b); }
     }
 }
